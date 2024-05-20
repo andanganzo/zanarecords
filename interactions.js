86669be1c763455d1e5dfc32a10f76fd46c966bd
Eric.k <166250140+andanganzo@users.noreply.github.com>
2024-05-20 11:08:27 +0200

    Update interactions.js

--- interactions.js
+++ interactions.js
@@ -1,24 +1,27 @@
-let navDiv = document.querySelector('.nav-div');
-var menuBarButton = document.querySelector('.menuBar-button');
-var logoH1 = document.querySelector('.logoH1');
-var inventorListNav = document.querySelector('.inventorListNav');
-let onOff = false;
+let navDiv = document.querySelector('.nav-div');//div located in html-header that will hold our menu
+var menuBarButton = document.querySelector('.menuBar-button');//button located in html-header that holds menu icon
+var logoH1 = document.querySelector('.logoH1');//img logo located in html-header
+var inventorListNav = document.querySelector('.inventorListNav');//nav element formed in JS 
+let onOff = false;//boolean that helps to track if menu is open or clossed
 var allInventors =  `
 
+    
     <div class="menu-Inventor-container">
+    <!--contains buttons that will help user to menu and navigate back-->
         <div class="BackCloseMenuBarButton-container">
+
             <button class="backMenuBarButton" onclick="
             openCloseMenuHome();
-
             "><img src="images/vecteezy_arrow-left-vector-icon-design_15001880-1.jpg" alt="">
             </button>
 
-
+            <!--condition that close menu-->
             <button class="closeMenuBarButton" onclick="
-                if(navDiv && onOff){
+                if(navDiv && !onOff){
                     navDiv.innerHTML = '';
                     onOff = false;
                 }
+                <!--if menu is closed, then logo and menu button will reappear-->
                 menuBarButton.style='opacity: 1';
                 logoH1.style='opacity: 1';
                 ">X</button>
@@ -62,48 +65,49 @@ if (navDiv && !onOff) {
           logoH1.style='opacity: 1';
       ">X</button>
 
-        <nav class='inventorListNav' id='nav-id'>
-            <a href='garrett-morgan.html'><li>Garret Augustus Morgan</li></a><br><br>
+      <nav class='inventorListNav' id='nav-id'>
+      <a href='garrett-morgan.html'><li>Garret Augustus Morgan</li></a><br><br>
 
-            <a href='tim-berners.html'><li>Tim Berners-Lee</li></a><br><br>
+      <a href='tim-berners.html'><li>Tim Berners-Lee</li></a><br><br>
 
-            <a href='gladys-west.html'><li>Gladys Mae West</li></a><br><br>
+      <a href='gladys-west.html'><li>Gladys Mae West</li></a><br><br>
 
-            <a href='marie-van-brittan.html'><li>Marie Van Brittan Brown</li></a><br><br>
+      <a href='marie-van-brittan.html'><li>Marie Van Brittan Brown</li></a><br><br>
 
-            <a href='charles-babbage.html'><li>Charles Babbage</li></a><br><br>
+      <a href='charles-babbage.html'><li>Charles Babbage</li></a><br><br>
 
-            <a href='frederic-mckinley.html'><li>Frederick McKinley Jones</li></a><br><br>
+      <a href='frederic-mckinley.html'><li>Frederick McKinley Jones</li></a><br><br>
 
-            <a href='alexandre-miles.html'><li>Alexander Miles</li></a><br><br>
+      <a href='alexandre-miles.html'><li>Alexander Miles</li></a><br><br>
 
-            <a href='lewis-lamer.html'><li>Lewis Latimer</li></a><br><br>
+      <a href='lewis-lamer.html'><li>Lewis Latimer</li></a><br><br>
 
-            <a href='grace-hopper.html'><li>Grace Hopper</li></a><br><br>
-        </nav> 
-  </div>`; onOff = true;
+      <a href='grace-hopper.html'><li>Grace Hopper</li></a><br><br>
+  </nav> 
+  </div>`;  
   
   } 
 }
 
-
+//the function of home page menu
 function openCloseMenuHome() {
   
-
     if (navDiv && !onOff) {
         navDiv.innerHTML = `
       <!--for disappiaring menu button and appear navLinks-->
       <div class="nav-Mother-Container"> 
+
+      <!--creates new button that closes menu inside navDiv-->
           <button class="closeMenuBarButton" onclick="
-          
-              if(navDiv && onOff){
+              if(navDiv && !onOff){
                   navDiv.innerHTML = '';
-                  onOff = false;
               }
+              <!--menu bar button and logo, will reappear-->
               menuBarButton.style='opacity: 1';
               logoH1.style='opacity: 1';
           ">X</button>
 
+          <!--ul contains "id" that allows to navigate to allInventors-->
           <ul id="ulNav-Mother-Container">
             <li onclick="
                 navDiv.innerHTML = allInventors;
@@ -112,37 +116,6 @@ function openCloseMenuHome() {
             <li><h3>Intertainment</h3></li>
             <li><h3>Politics</h3></li>
         </ul>
-      </div>`; onOff = true;
+      </div>`;
       }
     }
-
-    
-    
-
-
-
-
-
-
-// var prevScrollpos = window.pageYOffset;
-
-/* Get the header element and it's position */
-// var headerDiv = document.querySelector("header");
-// var headerBottom = headerDiv.offsetTop + headerDiv.offsetHeight;
-
-// window.onscroll = function() {
-// var currentScrollPos = window.pageYOffset;
-
-/* if scrolling down, let it scroll out of view as normal */
-// if (prevScrollpos <= currentScrollPos ){
-// headerDiv.classList.remove("fixedToTop");
-// headerDiv.style.top ="-7.2rem";
-// }
-/* otherwise if we're scrolling up, fix the nav to the top */
-// else{  
-// headerDiv.classList.add("fixedToTop");
-// headerDiv.style.top = "0";
-// }
-
-// prevScrollpos = currentScrollPos;
-// }
